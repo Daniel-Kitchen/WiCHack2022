6fb267731accf259c97dace65779fd28316a43b3
Danny <danielkitchen58@gmail.com>
2022-02-26 18:34:55 -0500

    Got button working to display a toast when pressed. This will lead to the ability to have it start the challenge and initiate useful variables.

--- app/src/main/java/com/example/wichack2022/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/wichack2022/ui/home/HomeFragment.kt
@@ -1,41 +1,61 @@
 package com.example.wichack2022.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.wichack2022.R
 import com.example.wichack2022.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+private var _binding: FragmentHomeBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+      _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    val textView: TextView = binding.textHome
+    homeViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
+    }
+      return root
+  }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
-        return root
+        // find the toast_button by its ID and set a click listener
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            // show the Toast
+            myToast.show()
+        }
+
     }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
