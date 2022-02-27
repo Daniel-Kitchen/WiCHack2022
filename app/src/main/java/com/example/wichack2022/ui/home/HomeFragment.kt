79446f5651f4dcfbf58d3f763a15600c9168e4a0
Danny <danielkitchen58@gmail.com>
2022-02-26 21:42:33 -0500

    Challenge can start and be incremented by hitting the check in button (will normally have this occur by hitting the check in button, but will be locked by a time of day check)

--- app/src/main/java/com/example/wichack2022/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/wichack2022/ui/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.example.wichack2022.ui.home
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
+import androidx.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +13,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.wichack2022.R
 import com.example.wichack2022.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -27,18 +30,30 @@ private var _binding: FragmentHomeBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val homeViewModel =
+      val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
       _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+      val root: View = binding.root
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
+      val textView: TextView = binding.textHome
+      homeViewModel.text.observe(viewLifecycleOwner) {
+        textView.text = it
+      }
+      view?.findViewById<TextView>(R.id.text_home2)?.text = ""
+
+      val sharedPreferences = context?.let { PreferenceManager.getDefaultSharedPreferences(it) }
+
+      val dayCount = sharedPreferences!!.getInt("day", 0)
+      if (dayCount != 0){
+          view?.let { createResume(it) }
+      }
       return root
   }
+
+    private fun createResume(view: View){
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,10 +62,27 @@ private var _binding: FragmentHomeBinding? = null
         }
         // find the toast_button by its ID and set a click listener
         view.findViewById<Button>(R.id.button).setOnClickListener {
-            // create a Toast with some text, to appear for a short time
-            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
-            // show the Toast
-            myToast.show()
+            if(view.findViewById<Button>(R.id.button).text == "Check in"){
+                //val sharedPreferences = requireContext().let { PreferenceManager.getDefaultSharedPreferences(it) }
+                val sharedPref = requireActivity().getSharedPreferences("com.example.wichack2022", Context.MODE_PRIVATE)
+
+                val dayCount = sharedPref.getInt("day", -1)+1
+                view.findViewById<TextView>(R.id.text_home2)?.text = dayCount.toString()
+                sharedPref.edit().putInt("day", dayCount).apply()
+
+            }
+            else{
+                view.findViewById<Button>(R.id.button).text = "Check in"
+                // create a Toast with some text, to appear for a short time
+                val myToast = Toast.makeText(context, "Challenge Started!", Toast.LENGTH_SHORT)
+                val sharedPref = requireActivity().getSharedPreferences("com.example.wichack2022", Context.MODE_PRIVATE)
+                sharedPref.edit().putInt("day", 1).apply()
+
+                view.findViewById<TextView>(R.id.text_home2)?.text = "1"
+                // show the Toast
+                myToast.show()
+            }
+
         }
 
     }
